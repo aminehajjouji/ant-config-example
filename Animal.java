72454cb9002bdc75879d19a7ab6b93cbb525d35e
Mohamed Amine Hajjouji <mahajjouji@SMB.SQLI.COM>
2022-03-14 14:43:38 +0100

    from cat branch

--- Animal.java
+++ Animal.java
@@ -2,7 +2,11 @@ public class Animal {
    
     public static void main(String[] args) {
         
-        String output ="";
+        String output ="Hello";
+        if(args[0].equals("cat")){
+            output="Meow";
+        }
+
         System.out.println(output);
     }
 }
