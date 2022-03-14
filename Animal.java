8c6c490f8ed13e1f4b113e939ba374dd0372f1eb
Mohamed Amine Hajjouji <mahajjouji@SMB.SQLI.COM>
2022-03-14 14:48:10 +0100

    commit from bird

--- Animal.java
+++ Animal.java
@@ -3,8 +3,8 @@ public class Animal {
     public static void main(String[] args) {
         
         String output ="Hello";
-        if(args[0].equals("cat")){
-            output="Meow";
+        if(args[0].equals("bird")){
+            output="tweet";
         }
 
         System.out.println(output);
